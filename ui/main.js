2b92348d8b230b021a4d0da3bc91cab25c19f0ea
TheRed142 <36451906+TheRed142@users.noreply.github.com>
2018-02-16 22:57:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,9 +10,9 @@ element.innerHTML = 'new value';
 var img = document.getElementById('madi');
 marginleft = 0;
 function moveright() {
-    marginleft = marginleft+10;
+    marginleft = marginleft+1;
     img.style.marginleft = marginleft + 'px';
 }
   img.onclick = function() {
-  var inerval = setInterval(moveright,100);
+  var inerval = setInterval(moveright,50);
 };
