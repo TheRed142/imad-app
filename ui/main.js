d67c196a1057615ccc974357b3356b753d15b8b4
TheRed142 <36451906+TheRed142@users.noreply.github.com>
2018-02-16 22:31:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,3 +6,9 @@ var element = document.getElementById('main-text');
 
 element.innerHTML = 'new value';
 
+//movig the image
+var img = document.getElementById('img');
+img.onclick = function(){
+  img.style.marginleft = '100px';
+  
+};
