44e72dda60764952d1287046e55f71b0036fd74a
TheRed142 <36451906+TheRed142@users.noreply.github.com>
2018-02-20 04:59:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 
-element.innerHTML = 'new value';
+element.innerHTML = '';
 
 //movig the image
 var img = document.getElementById('madi');
