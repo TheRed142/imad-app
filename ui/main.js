635063b70d8d8dcb397c59b16c7029f5d93b36d4
TheRed142 <36451906+TheRed142@users.noreply.github.com>
2018-02-20 05:22:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,16 @@
-console.log('Loaded!');
- 
-//changing the content
+//counter code
+var button = document.getElementById('counter');
+var counter = 0;
 
-var element = document.getElementById('main-text');
-
-element.innerHTML = 'THE RED';
-
-//movig the image
-var img = document.getElementById('madi');
-marginleft = 0;
-function moveright() {
-    marginleft = marginleft+1;
-    img.style.marginleft = marginleft + 'px';
-}
-  img.onclick = function() {
-  var inerval = setInterval(moveright,50);
+button.onclick = function() {
+   
+   
+  //make a request to the counter end point
+  
+  //capture the respond and store it in a variable
+  
+  //render the variable in the correct span
+  counter = counter+1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
 };
