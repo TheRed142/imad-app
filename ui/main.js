3e0c4f1884376684654ec15941a80e7124ee3930
TheRed142 <36451906+TheRed142@users.noreply.github.com>
2018-02-20 22:12:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,6 +20,6 @@ button.onclick = function() {
     //not done yet
   };
   //make the request
-  request.open('GET','http:likh17is.imad.hasura-app.io/counter', true);
+  request.open('GET','http://likh17is.imad.hasura-app.io/counter', true);
   request.send(null);
 };
