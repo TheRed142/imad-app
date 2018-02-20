9a7ff3374def08047d3f379ffda9b0c449901f69
TheRed142 <36451906+TheRed142@users.noreply.github.com>
2018-02-20 23:34:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get('/ui/madi.png', function (req, res) {
 app.get('/submit-name/:name', function(req, res) {
    //get the name from the request object
    var name = req.params.name; 
-   names.push('name');
+   names.push(name);
    //JSON: javascript object notation
    res.send(JSON.stringify(names));//TOOO
 });
